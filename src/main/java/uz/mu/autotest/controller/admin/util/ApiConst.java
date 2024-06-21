37962bbd1acc1583cbe76f1e515476d3ab37dcdc
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-06-21 15:27:07 +0500

    feat: add api const

---
+++ src/main/java/uz/mu/autotest/controller/admin/util/ApiConst.java
@@ -0,0 +1,8 @@
+package uz.mu.autotest.controller.admin.util;
+
+public class ApiConst {
+    public static final String ADMINS_PREFIX = "/admins";
+    public static final String STUDENTS_PREFIX = "/students";
+    public static final String TEACHERS_PREFIX = "/teachers";
+    public static final String GROUPS_PREFIX = "/groups";
+}

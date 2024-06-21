9930abc284d4d55a4a7b32205852d6a529e20927
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-06-21 15:07:33 +0500

    upd: updated UserService

--- src/main/java/uz/mu/autotest/service/UserService.java
+++ src/main/java/uz/mu/autotest/service/UserService.java
@@ -1,31 +1,28 @@
 package uz.mu.autotest.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import uz.mu.autotest.model.AuthProvider;
 import uz.mu.autotest.model.User;
 import uz.mu.autotest.repository.UserRepository;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    public void createUserAfterSuccessfullOAuth2Login(String login, String displayName, AuthProvider authProvider) {
+    public void createUserAfterSuccessfullOAuth2Login(String login, String displayName) {
         User user = new User();
         user.setUsername(login);
-        user.setName(displayName);
-        user.setEnabled(true);
-        user.setAuthProvider(authProvider);
-
+        user.setFirstName(displayName);
+        user.setActive(true);
         userRepository.save(user);
     }
 
     public Optional<User> getByUsername(String username) {
-        return userRepository.findByUsername(username);
+        return userRepository.findByUsernameWithRoles(username);
     }
-
 }
+

5fe2252eea35e0218168ecd4ac193e4c8e2121b4
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-07-05 00:12:16 +0500

    feat: update WebsocketController

--- src/main/java/uz/mu/autotest/controller/WebsocketController.java
+++ src/main/java/uz/mu/autotest/controller/WebsocketController.java
@@ -2,10 +2,7 @@ package uz.mu.autotest.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,6 +13,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uz.mu.autotest.client.GithubClient;
+import uz.mu.autotest.dto.AttemptDto;
 import uz.mu.autotest.dto.SubmitTaskRequest;
 import uz.mu.autotest.dto.UserTakenLabDto;
 import uz.mu.autotest.service.AuthService;
@@ -26,7 +24,6 @@ import uz.mu.autotest.service.impl.GithubRepositoryService;
 import uz.mu.autotest.service.impl.GithubSubmitTaskService;
 
 import java.security.Principal;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/github")
@@ -45,39 +42,40 @@ public class WebsocketController {
     private final SimpMessagingTemplate simpMessagingTemplate;
 
 
-    @MessageMapping("/submitTask")
-    public void submitTask(SubmitTaskRequest request, @AuthenticationPrincipal Principal principal, @Payload String username, @Headers Map<String, String> headers, SimpMessageHeaderAccessor headerAccessor) throws InterruptedException {
-        log.info("headers: {}", headers);
-        log.info("principalGetName: {}", principal.getName());
-        log.info("username: {}", username);
-        log.info("#############################TRIGGERED####################");
-        String sessionId = headers.get("simpSessionId");
-        log.info("sessionId: {}", sessionId);
-        // Check if the flag is set in the session attribute
-        if (headerAccessor.getSessionAttributes().get("connectionEstablished") == null) {
-            // Flag is not set, set it to true
-            headerAccessor.getSessionAttributes().put("connectionEstablished", true);
-
-            // Your existing logic here
-            Thread.sleep(1000);
-            OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) principal;
-            Thread.sleep(5000);
-            simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/queue/attempts", new SubmitTaskRequest(1L));
-        }
-    }
-
-
+    // TODO implement RabbitMQ interaction for submitTask
 //    @MessageMapping("/submitTask")
-//    @SendToUser("/queue/attempts")
-//    public AttemptDto submitTask(SubmitTaskRequest request, @AuthenticationPrincipal Principal principal) throws InterruptedException {
-//        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) principal;
-//        OAuth2User oAuth2User = oAuth2AuthenticationToken.getPrincipal();
-//        String login = oAuth2User.getAttribute("login").toString();
-//        OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId(), oAuth2AuthenticationToken.getName());
-//        String accessToken = "Bearer " + client.getAccessToken().getTokenValue();
+//    public void submitTask(SubmitTaskRequest request, @AuthenticationPrincipal Principal principal, @Payload String username, @Headers Map<String, String> headers, SimpMessageHeaderAccessor headerAccessor) throws InterruptedException {
+//        log.info("headers: {}", headers);
+//        log.info("principalGetName: {}", principal.getName());
+//        log.info("username: {}", username);
+//        log.info("#############################TRIGGERED####################");
+//        String sessionId = headers.get("simpSessionId");
+//        log.info("sessionId: {}", sessionId);
+//        // Check if the flag is set in the session attribute
+//        if (headerAccessor.getSessionAttributes().get("connectionEstablished") == null) {
+//            // Flag is not set, set it to true
+//            headerAccessor.getSessionAttributes().put("connectionEstablished", true);
 //
-//        return githubSubmitTaskService.submitTask(request, login, accessToken);
+//            // Your existing logic here
+//            Thread.sleep(1000);
+//            OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) principal;
+//            Thread.sleep(5000);
+//            simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/queue/attempts", new SubmitTaskRequest(1L));
+//        }
 //    }
+
+
+    @MessageMapping("/submitTask")
+    @SendToUser("/queue/attempts")
+    public AttemptDto submitTask(SubmitTaskRequest request, @AuthenticationPrincipal Principal principal) throws InterruptedException {
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) principal;
+        OAuth2User oAuth2User = oAuth2AuthenticationToken.getPrincipal();
+        String login = oAuth2User.getAttribute("login").toString();
+        OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(oAuth2AuthenticationToken.getAuthorizedClientRegistrationId(), oAuth2AuthenticationToken.getName());
+        String accessToken = "Bearer " + client.getAccessToken().getTokenValue();
+
+        return githubSubmitTaskService.submitTask(request, login, accessToken);
+    }
 
     @MessageMapping("/generateRepository")
     @SendToUser("/queue/repositories")

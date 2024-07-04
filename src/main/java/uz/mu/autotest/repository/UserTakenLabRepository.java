228bb37bde5c1fd5fea488532d34853240eae63f
Asliddin Eshonkulov <eshonkulov.asliddin.mars@gmail.com>
2024-07-05 00:06:49 +0500

    feat: add native query

--- src/main/java/uz/mu/autotest/repository/UserTakenLabRepository.java
+++ src/main/java/uz/mu/autotest/repository/UserTakenLabRepository.java
@@ -1,6 +1,7 @@
 package uz.mu.autotest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uz.mu.autotest.model.UserTakenLab;
 
@@ -10,6 +11,11 @@ import java.util.Optional;
 @Repository
 public interface UserTakenLabRepository extends JpaRepository<UserTakenLab, Long> {
 
+    @Query("SELECT utl FROM UserTakenLab utl " +
+            "JOIN utl.user u " +
+            "JOIN utl.course c " +
+            "WHERE c.id = :courseId " +
+            "AND u.loginName = :username")
     List<UserTakenLab> findByCourseIdAndUserUsername(Long courseId, String username);
     Optional<UserTakenLab> findById(Long id);
 }

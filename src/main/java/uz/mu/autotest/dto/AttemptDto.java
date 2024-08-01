3c648cefb0ecd5ab56686584b894bdaca8c7f1e5
asldev <eshonkulov.asliddin.mars@gmail.com>
2024-08-01 08:12:44 +0500

    feat: update AttemptDto

--- src/main/java/uz/mu/autotest/dto/AttemptDto.java
+++ src/main/java/uz/mu/autotest/dto/AttemptDto.java
@@ -1,10 +1,12 @@
 package uz.mu.autotest.dto;
 
+import uz.mu.autotest.dto.testsuite.TestSuiteDto;
+
 public record AttemptDto (
         Long id,
         Long runNumber,
         String detailsUrl,
         String result,
         Long userTakenLabId,
-        Long testSuiteEntityId ) {
+        TestSuiteDto testSuiteDto) {
 }

2424dfbbe788cc597f2a8a5a7f06280e5e8ae36e
asldev <eshonkulov.asliddin.mars@gmail.com>
2024-08-01 20:36:44 +0500

    feat: update repositories

--- src/main/java/uz/mu/autotest/repository/TestSuiteEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package uz.mu.autotest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.mu.autotest.model.TestSuiteEntity;
-
-public interface TestSuiteEntityRepository extends JpaRepository<TestSuiteEntity, Long> {
-}

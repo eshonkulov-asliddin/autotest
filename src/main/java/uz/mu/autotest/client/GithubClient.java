2b82921ff6cf3ba6a8eaf8d0ceb84f60cf67e1a8
asldev <eshonkulov.asliddin.mars@gmail.com>
2024-08-02 22:49:25 +0500

    feat: update client

--- src/main/java/uz/mu/autotest/client/GithubClient.java
+++ src/main/java/uz/mu/autotest/client/GithubClient.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -20,6 +22,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GithubClient {
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
@@ -29,7 +32,7 @@ public class GithubClient {
 
     public ResponseEntity<String> triggerWorkflow(String owner, String repo, String accessToken) {
 
-        HttpHeaders headers = getHeaders(accessToken);
+        HttpHeaders headers = buildHeaders(accessToken);
         String body = "{\"ref\":\"master\"}";
 
         HttpEntity<String> entity = new HttpEntity<>(body, headers);
@@ -38,14 +41,40 @@ public class GithubClient {
         return restTemplate.exchange(triggerWorkflowUrl, HttpMethod.POST, entity, String.class);
     }
 
+    @SneakyThrows
     public Optional<Attempt> getLastActionRun(String owner, String repo, String accessToken) {
+        Thread.sleep(5000); // wait for the workflow to finish
         String actionsRunsUrl = githubApiUrl + "/repos/" + owner + "/" + repo + "/actions/runs";
-        var headers = getHeaders(accessToken);
+        var headers = buildHeaders(accessToken);
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<String> actionsRunsResponse = restTemplate.exchange(actionsRunsUrl, HttpMethod.GET, entity, String.class);
         String bodyJSON = actionsRunsResponse.getBody();
         try {
             JsonNode lastWorkflowRun = objectMapper.readTree(bodyJSON).get("workflow_runs").get(0);
+            log.info("Last workflow run: {}", lastWorkflowRun);
+            int runNumber = lastWorkflowRun.get("run_number").intValue();
+            String conclusion = lastWorkflowRun.get("conclusion").textValue();
+            String detailsURL = lastWorkflowRun.get("html_url").textValue();
+            Attempt attempt = new Attempt();
+            attempt.setRunNumber((long) runNumber);
+            attempt.setResult(conclusion.equals("failure") ? Result.FAILURE : Result.SUCCESS);
+            attempt.setDetailsUrl(detailsURL);
+            return Optional.of(attempt);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Attempt> getLastActionRunByRunId(String owner, String repo, String accessToken, long runId) {
+        String actionsRunsUrl = githubApiUrl + "/repos/" + owner + "/" + repo + "/actions/runs/" + runId;
+        var headers = buildHeaders(accessToken);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> actionsRunsResponse = restTemplate.exchange(actionsRunsUrl, HttpMethod.GET, entity, String.class);
+        String bodyJSON = actionsRunsResponse.getBody();
+        try {
+            JsonNode lastWorkflowRun = objectMapper.readTree(bodyJSON);
+            log.info("Last workflow run: {}", lastWorkflowRun);
             int runNumber = lastWorkflowRun.get("run_number").intValue();
             String conclusion = lastWorkflowRun.get("conclusion").textValue();
             String detailsURL = lastWorkflowRun.get("html_url").textValue();
@@ -62,7 +91,7 @@ public class GithubClient {
 
     public Optional<String> getLastActionRunDownloadArtifactUrl(String owner, String repo, String accessToken) {
         String actionsRunsUrl = githubApiUrl + "/repos/" + owner + "/" + repo + "/actions/artifacts";
-        var headers = getHeaders(accessToken);
+        var headers = buildHeaders(accessToken);
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<String> actionsRunsResponse = restTemplate.exchange(actionsRunsUrl, HttpMethod.GET, entity, String.class);
         String bodyJSON = actionsRunsResponse.getBody();
@@ -78,7 +107,7 @@ public class GithubClient {
     }
 
     public ResponseEntity<String> generateRepository(String labOwner, String templateRepoOwner, String templateRepo, String description, boolean doIncludeAllBranch, boolean isPrivate, String accessToken) {
-        HttpHeaders headers = getHeaders(accessToken);
+        HttpHeaders headers = buildHeaders(accessToken);
         String requestBody = generateRepoRequestBody(labOwner, templateRepo, description, doIncludeAllBranch, isPrivate);
 
         HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
@@ -98,7 +127,7 @@ public class GithubClient {
         return requestBody;
     }
 
-    private HttpHeaders getHeaders(String accessToken){
+    private HttpHeaders buildHeaders(String accessToken){
         HttpHeaders headers = new HttpHeaders();
         headers.set("Accept", "application/vnd.github+json");
         headers.set("Authorization", accessToken);

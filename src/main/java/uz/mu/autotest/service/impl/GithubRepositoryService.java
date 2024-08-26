3127c0278301d3c2b4cf164d34ff9f53d54c7412
asldev <eshonkulov.asliddin.mars@gmail.com>
2024-08-26 11:20:26 +0500

    feat: add detailed log

--- src/main/java/uz/mu/autotest/service/impl/GithubRepositoryService.java
+++ src/main/java/uz/mu/autotest/service/impl/GithubRepositoryService.java
@@ -65,7 +65,8 @@ public class GithubRepositoryService implements RepositoryService {
 
             return new StudentTakenLabDto(studentTakenLab.getId(), studentTakenLab.getGithubUrl(), String.valueOf(studentTakenLab.getStatus()), studentTakenLab.getCourse().getId(), studentTakenLab.getLab().getId(), studentTakenLab.getUser().getId());
         }catch (HttpClientErrorException e){
-            throw new GenerateRepositoryException(String.format("Failed to generate repository, because repository %s already exists", templateRepo));
+            log.error("Error generating repo: {}", e.toString());
+            throw new GenerateRepositoryException(String.format("Failed to generate repository: %s, cause: %s", templateRepo, e));
         }
     }
 }
